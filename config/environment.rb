2a6e8a149092ca28c6d29e1b9bcf57b788952eb8
Ben Purinton <ben.purinton@gmail.com>
2023-07-17 15:08:48 -0800

    cleanup, fix quotes

--- config/environment.rb
+++ config/environment.rb
@@ -1,14 +1,12 @@
-require './app'
+require "./app"
 
 configure :development do
-  require "sinatra/reloader"
-
   # we would also like a nicer error page in development
-  require 'better_errors'
-  require 'binding_of_caller'
-  
+  require "better_errors"
+  require "binding_of_caller"
+
   # need this configure for better errors
   use(BetterErrors::Middleware)
   BetterErrors.application_root = __dir__
-  BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
+  BetterErrors::Middleware.allow_ip!("0.0.0.0/0.0.0.0")
 end
